9efbdcf8e46c362f20a3cb5f4f6607b9a3ff354e
Tyler Cummings <tylercummings@comcast.net>
2019-04-22 20:10:59 -0400

    autohey command listens to a voice channel and plays hey.mp3 when people join

---
+++ commands/text/autohey.js
@@ -0,0 +1,29 @@
+const BaseTextCommand = require('../../base_text_command')
+
+class AutoHey extends BaseTextCommand {
+  constructor(client) {
+    super(client, {
+      name: 'autohey',
+      group: 'text',
+      memberName: 'autohey',
+      description: 'Asks hey how\'s it going when somebody joins'
+    })
+  }
+
+  async run(message, args) {
+    var { member, guild } = message
+    if (!servers[guild.id]) servers[guild.id] = {}
+    if (args === 'stop') {
+      Object.assign(servers[guild.id], { autoHey: false })
+    } else {
+      if (member.voiceChannel) {
+        Object.assign(servers[guild.id], {
+          autoHey: { voiceChannelId: member.voiceChannel.id }
+        })
+        message.channel.sendMessage(`Watching ${member.voiceChannel.name} for joins`)
+      }
+    }
+  }
+}
+
+module.exports = AutoHey
--- index.js
+++ index.js
@@ -26,3 +26,26 @@ client.registry
 
 client.login(config.token)
   .then(console.log('Ready'))
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  var newUserChannel = newMember.voiceChannel
+  var oldUserChannel = oldMember.voiceChannel
+
+  if (oldUserChannel === undefined && newUserChannel !== undefined) {
+    var server = global.servers[newUserChannel.guild.id]
+    if (server && server.autoHey && server.autoHey.voiceChannelId === newUserChannel.id) {
+      newUserChannel.join()
+        .then(connection => {
+          // Very hacky, need a way to call a command directly
+          var filePath = `${__dirname}/sounds/hey/hey.mp3`
+          server.dispatcher = connection.playFile(filePath)
+
+          server.dispatcher.on('end', () => {
+            setTimeout(() => connection.disconnect(), 3000)
+          })
+        })
+    }
+  } else if (newUserChannel === undefined){
+    console.log('leave')
+  }
+})
