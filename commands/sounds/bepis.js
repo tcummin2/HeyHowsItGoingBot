2059aa9f41ed0010e2aa8bf8ae77e4fa532a2b58
Tyler Cummings <tylercummings@comcast.net>
2019-06-10 22:54:57 -0400

    More bepis

--- commands/sounds/bepis.js
+++ commands/sounds/bepis.js
@@ -8,7 +8,7 @@ class Bepis extends BaseSoundCommand {
       memberName: 'bepis',
       description: 'Gimme the bepis'
     })
-    this.fileNames = ['bepis.mp3']
+    this.fileNames = ['bepis.mp3', 'bepis-2.mp3', 'bepis-3.mp3', 'bepis-4.mp3', 'bepis-5.mp3', 'bepis-6.mp3', 'bepis-7.mp3']
   }
 }
 
