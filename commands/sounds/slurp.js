0c05a4a7280954c87510d2ea44760acd11e3cea5
Tyler Cummings <tylercummings@comcast.net>
2020-01-19 15:24:05 -0500

    Added !slurp

---
+++ commands/sounds/slurp.js
@@ -0,0 +1,15 @@
+const BaseSoundCommand = require('../../base_sound_command')
+
+class Slurp extends BaseSoundCommand {
+  constructor(client) {
+    super(client, {
+      name: 'slurp',
+      group: 'sounds',
+      memberName: 'slurp',
+      description: 'Numnumnumnumnumnum'
+    })
+    this.fileNames = ['slurp.mp3', 'slurp-2.mp3', 'slurp-3.mp3', 'slurp-4.mp3']
+  }
+}
+
+module.exports = Slurp
