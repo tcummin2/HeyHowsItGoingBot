58321d4a7e86a17968c97f4b665ea0002b879bf3
Tyler Cummings <tylercummings@comcast.net>
2020-04-07 19:04:35 -0400

    Add messaging and channel category movement for !quarantine

--- commands/other/quarantine.js
+++ commands/other/quarantine.js
@@ -13,12 +13,42 @@ class Quarantine extends BaseSoundCommand {
     this.fileNames = ['tactical-nuke.mp3']
   }
 
-  async run({ member: { voiceChannel } }) {
-    super.run(...arguments).then(() => {
-      if (voiceChannel && !voiceChannel.name.includes(QUARANTINED_NAME)) {
-        voiceChannel.setName(`☣️ ${QUARANTINED_NAME} ${voiceChannel.name}`)
+  reportQuarantine(guild, oldName, args) {
+    var scoreboard = guild.channels.find(({ name }) => name.toLowerCase().includes('scoreboard'))
+    if (scoreboard) {
+      var message = `${oldName} was ☣️ QUARANTINED ☣️`
+      if (args) {
+        message = `${message} due to ${args}`
       }
-    })
+      scoreboard.send(message)
+    }
+  }
+
+  moveChannelAndReport(voiceChannel, oldName, args) {
+    var { guild } = voiceChannel
+    var quarantineCategory = guild.channels.filter(({ type }) => type === 'category')
+      .find(({ name }) => name.toLowerCase().includes('quarantine'))
+
+    if (quarantineCategory) {
+      voiceChannel.setParent(quarantineCategory)
+    }
+
+    this.reportQuarantine(guild, oldName, args)
+  }
+
+  async run({ member: { voiceChannel }, channel }, args) {
+    if (!voiceChannel) return
+
+    if (!voiceChannel.name.includes(QUARANTINED_NAME)) {
+      super.run(...arguments)
+        .then(() => {
+          const voiceChannelName = voiceChannel.name
+          voiceChannel.setName(`☣️ ${QUARANTINED_NAME} ${voiceChannelName}`)
+            .then(channel => this.moveChannelAndReport(channel, voiceChannelName, args))
+        })
+    } else {
+      channel.send(`${voiceChannel.name} is already ☣️ QUARANTINED ☣️!`)
+    }
   }
 }
 
