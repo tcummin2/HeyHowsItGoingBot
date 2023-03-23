c74dc76142f22a907cce829f507692c8903993f8
Tyler Cummings <tylercummings@comcast.net>
2023-03-22 21:56:14 -0400

    Temporarily take down the bot

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-const { Client } = require('discord.js-commando')
+const { Client, GatewayIntentBits } = require('discord.js')
+// const { Client, GatewayIntentBits } = require('discord.js-commando')
 const config = require('./config.json')
 
 global.servers = {}
 
 const client = new Client({
   owner: config.ownerId,
-  unknownCommandResponse: false
+  unknownCommandResponse: false,
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 })
 
-client.registry
-  .registerDefaultTypes()
-  .registerDefaultGroups()
-  .registerGroups([
-    ['text', 'Text'],
-    ['sounds', 'Sounds'],
-    ['image', 'Image'],
-    ['hey', 'Hey'],
-    ['tts', 'Tts'],
-    ['hidden', 'Hidden'],
-    ['other', 'Other']
-  ])
-  .registerDefaultCommands({
-    ping: false,
-    unknownCommand: false,
-    commandState: false
-  })
-  .registerCommandsIn(`${__dirname}/commands`)
+// client.registry
+//   .registerDefaultTypes()
+//   .registerDefaultGroups()
+//   .registerGroups([
+//     ['text', 'Text'],
+//     ['sounds', 'Sounds'],
+//     ['image', 'Image'],
+//     ['hey', 'Hey'],
+//     ['tts', 'Tts'],
+//     ['hidden', 'Hidden'],
+//     ['other', 'Other']
+//   ])
+//   .registerDefaultCommands({
+//     ping: false,
+//     unknownCommand: false,
+//     commandState: false
+//   })
+//   .registerCommandsIn(`${__dirname}/commands`)
 
 client.login(config.token)
   .then(console.log('Ready'))
 
 client.on('error', console.error)
+
+client.on('messageCreate', async ({ channel, content, author }) => {
+  if (content.startsWith('!') && !author.bot) {
+    await channel.send({ content: 'shut up' })
+  }
+})
